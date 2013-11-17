8564c07ccdb8175edf6e5b0652343d13c7e650af
BaBaBags <ryan@ryanbagwell.com>
2013-11-17 15:40:57 -0600

    adding 1.6 compatibility

--- paypal/standard/ipn/urls.py
+++ paypal/standard/ipn/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import *
+try:
+	#for Django >= 1.6
+	from django.conf.urls import *
+except:
+	#for Django < 1.6
+	from django.conf.urls.defaults import *
 
 urlpatterns = patterns('paypal.standard.ipn.views',            
     url(r'^$', 'ipn', name="paypal-ipn"),
